3595969897310424546b32fa53066c044883f1fa
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-03-02 23:28:14 -0500

    Tokenize queries before sending to the database

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -8,7 +8,11 @@ class Search_Controller extends Base_Controller {
         $query = Input::get('q');
         $results = null;
         if (isset($query)) {
-            $results = Record::where('data', 'LIKE', '%' . $query . '%')->get();
+            $results = Record::where(function($dbquery) use ($query) {
+                foreach (explode(' ', $query) as $keyword) {
+                    $dbquery->where('data', 'LIKE', '%' . $keyword . '%');
+                }
+            })->get();
         }
         Asset::add('fieldstyles', 'css/fields.css');
 		return View::make('interface.search')->with('results', $results)->with('query', $query);
