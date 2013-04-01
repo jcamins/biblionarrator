66578fa8d5904b4c88cfc4073184a2c373896f5f
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-03-31 22:46:16 -0400

    Add administration home page

---
+++ application/views/admin/index.blade.php
@@ -0,0 +1,22 @@
+@layout('layouts/main')
+
+@section('toolbar')
+<h3>Administer Biblionarrator</h3>
+@endsection
+
+@section('content')
+<ul class="home-links">
+    @if ( Authority::can('manage', 'Field') )
+    <li><a href="/admin/field">Fields</a></li>
+    @endif
+    @if ( Authority::can('manage', 'User') )
+    <li><a href="/admin/user">Users</a></li>
+    @endif
+    @if ( Authority::can('manage', 'Collection') )
+    <li><a href="/admin/collection">Collections</a></li>
+    @endif
+    @if ( Authority::can('manage', 'RecordType') )
+    <li><a href="/admin/recordtype">Record types</a></li>
+    @endif
+</ul>
+@endsection
