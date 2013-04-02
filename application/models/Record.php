b032351bf8e8cf504b7d9105158e93dfaa003f0e
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-04-02 10:45:58 -0400

    Start work on the PHP JSON<->HTML conversion

--- application/models/Record.php
+++ application/models/Record.php
@@ -21,6 +21,9 @@ class Record extends Eloquent
 {
     public static $timestamps = true;
 
+    protected static $htmlelements = array('a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col', 'colgroup', 'command', 'datalist', 'dd', 'del', 'details', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'map', 'mark', 'menu', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr');
+    protected static $allowedattrs = array('role', 'itemscope', 'itemtype', 'itemid', 'itemprop', 'itemref');
+
     public function primaries() {
         return $this->has_many('Primary', 'record_id');
     }
@@ -89,6 +92,44 @@ class Record extends Eloquent
         } else {
             return $this->data;
         }
+    }
+
+    protected function _raw2html() {
+    }
+
+    static function traverseRaw($object = null, $format = 'html') {
+        $output = '';
+        $html = (strpos($format, 'html') !== false);
+        if (is_string($object)) {
+            $output = $object;
+        } else if (!is_null($object)) {
+            foreach ($object as $elem => $obj) {
+                $htmlelem = $elem;
+                if ($html && in_array($elem, $htmlelements)) {
+                    if (isset($obj['link']) && strpos($format, 'nolink') === false) {
+                        $htmlelem = 'a';
+                    } else {
+                        $htmlelem = 'span';
+                    }
+                    $output .= '<' . $htmlelem . ' class="' . $elem . '"';
+                } else {
+                    $output += '<' + $elem;
+                }
+                foreach ($obj as $attr => $val) {
+                    if ($html && in_array($attr, $allowedattrs) && isset($val)) {
+                        $output .= ' ' . $attr . '="' . $val . '"';
+                    } else if ($attr !== 'children') {
+                        $output .= ' ' . $attr . '="' . $val . '"';
+                    }
+                }
+                $output .= '>';
+                foreach ($obj['children'] as $child) {
+                    $output .= traverseRaw($child, $format);
+                }
+                $output .= '</' . $htmlelem . '>';
+            }
+        }
+        return $output;
     }
 
     public function _crosswalkRecordXml ($stylesheet) {
