04f69351dc8a49d6e1052d4c06587e64aaca3703
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-04-20 14:11:17 -0400

    Replace html5shiv with document.createElement
    This is necessary to have rangy work with IE < 9. IE can die in a
    fire!

--- application/views/layouts/main.blade.php
+++ application/views/layouts/main.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ $title }}</title>
-        <script src="/js/html5shiv.js"></script>
-        <script src="/js/html5shiv-printshiv.js"></script>
+        <script type="text/javascript">
+            for(var e,l='article aside footer header nav section time'.split(' ');e=l.pop();document.createElement(e))
+        </script>
         <!--[if lt IE 9]>
             <link rel="stylesheet" type="text/css" href="/css/style-ie.css" />
         <![endif]-->
