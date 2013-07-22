f4fba368be59a2c55e1b0aa7d002e266f904ac80
Jared Camins-Esakov <jcamins@gmail.com>
2013-07-22 09:16:22 -0400

    Use an object for search paging

--- public/js/list.js
+++ public/js/list.js
@@ -29,10 +29,40 @@ function initializeList() {
     });
     $('body').on('click', '.explore-links', null, function() {
         var row = $(this).parents('tr');
-        $.ajax({
-            url: '/record/' + $(row).attr('data-id') + '/links'
-        }).done(function (data) {
-            if ($(row).parents('.leftPane').length > 0) {
+        window.bnpanes.next('/record/' + $(row).attr('data-id') + '/links', row);
+    });
+}
+
+/* The built-in caching from the web browser will almost certainly be sufficient,
+   but implementing our own history/garbage collection is useful for tracking
+   what the system is doing while we're trying to refactor search panes. */
+(function( bnpanes, $, undefined ) {
+    var panes = { };
+    var current;
+    var previous = [ ];
+    var next = [ ];
+    var history = [ ];
+    
+    bnpanes.historysize = 4;
+    bnpanes.viewportwidth = 2;
+    
+    bnpanes.previous = function (url, reference) {
+        if (url) {
+            previous = bnpanes.gc(previous, 0);
+        } else {
+            url = previous.shift;
+        }
+        me.load(url);
+    };
+        
+    bnpanes.next = function (url, reference) {
+        if (url) {
+            next = bnpanes.gc(next, 0);
+        } else {
+            url = next.shift;
+        }
+        bnpanes.load(url, function (data) {
+            if ($(reference).parents('.leftPane').length > 0) {
                 $('tr.browsing').removeClass('browsing');
                 $('.rightPane').remove();
             } else {
@@ -40,8 +70,37 @@ function initializeList() {
                 $('.leftPane').remove();
                 $('.rightPane').removeClass('rightPane').addClass('leftPane').css('margin-top', '0');
             }
-            $(row).addClass('browsing');
+            $(reference).addClass('browsing');
             $('.search-scroller').append('<div class="rightPane" style="margin-top: ' + $(window).scrollTop() + 'px">' + data + '</div>');
         });
-    });
-}
+    };
+    
+    bnpanes.load = function (url, render) {
+        if (panes[url]) {
+            render(panes[url]);
+        } else {
+            $.ajax({
+                url: url,
+                success: function (data) {
+                    panes[url] = data;
+                    history.unshift(url);
+                    bnpanes.gc();
+                    render(data);
+                },
+                failure: function (data) {
+                }
+            });
+        }
+    };
+    
+    bnpanes.gc = function (list, length) {
+        list = list || history;
+        length = length || bnpanes.historysize;
+        var url;
+        while (list.length > length) {
+            url = list.pop();
+            delete panes[url];
+        }
+        return list;
+    };
+}( window.bnpanes = window.bnpanes || {}, jQuery ));
