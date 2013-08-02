adcd1f6346385cd328ad46fd00489226c076d5dc
Jared Camins-Esakov <jcamins@gmail.com>
2013-08-01 21:20:30 -0400

    Remove obsolete templates and PHP list controllers

--- application/views/components/result-pane-common.blade.php
+++
@@ -1,1 +0,0 @@
-<button title="Show preview" class="preview btn btn-link"><i class='icon-eye-open'></i></button>
--- application/views/components/result-toolbar-common.blade.php
+++
@@ -1,3 +0,0 @@
-<button title="Comment" class="add-comment btn btn-link"><i class="icon-comment"></i></button>
-<a href="/bookmarks/add/{{ $record->id }}" title="Bookmark" class="add-bookmark btn btn-link"><i class="icon-bookmark"></i></a>
-<button title="Hide preview" disabled="disabled" class="hide-preview btn btn-link"><i class='icon-chevron-up'></i></button>
