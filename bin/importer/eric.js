7363fd2f292dd9af75c545fc321c25e4125caa1a
Jared Camins-Esakov <jcamins@gmail.com>
2013-08-03 14:02:45 -0400

    Add new eric format and fix SQL

---
+++ bin/importer/eric.js
@@ -0,0 +1,120 @@
+var fs = require('fs'),
+    xml2js = require('xml2js'),
+    inspect = require('eyes').inspector({maxLength: false}),
+    datastore = require('../../lib/datastore'),
+    Q = require('q');
+
+var parser = new xml2js.Parser();
+var recs = { };
+var promises = [ ];
+fs.readFile(process.argv[2], function(err, data) {
+    parser.parseString(data, function (err, result) {
+        var record;
+        for (var ii in result.records.record) {
+            record = result.records.record[ii];
+            var rec = {
+                title: record.metadata[0]['dc:title'][0],
+                source: record.metadata[0]['dc:source'][0],
+                citation: record.metadata[0]['eric:citation'][0],
+                description: record.metadata[0]['dc:description'][0],
+                publisher: record.metadata[0]['dc:publisher'][0],
+                subjects: record.metadata[0]['dc:subject'],
+                types: record.metadata[0]['dc:type'],
+            };
+            for (var jj in record.metadata[0]['dc:identifier']) {
+                if (record.metadata[0]['dc:identifier'][jj]['$'].scheme === 'dcterms:URI') {
+                    rec.doi = record.metadata[0]['dc:identifier'][jj]['_'];
+                } else if (record.metadata[0]['dc:identifier'][jj]['$'].scheme === 'eric_accno') {
+                    rec.accno = record.metadata[0]['dc:identifier'][jj]['_'];
+                }
+            }
+            rec.creators = [];
+            for (var jj in record.metadata[0]['dc:creator']) {
+                if (record.metadata[0]['dc:creator'][jj]['_']) {
+                    rec.creators.push(record.metadata[0]['dc:creator'][jj]['_']);
+                    if (record.metadata[0]['dc:creator'][jj]['$'].scheme === 'personal author') {
+                        promises.push(addRecord({ "article":{ "children":[ { "header":{ "children":[ { "span":{ "children":[ record.metadata[0]['dc:creator'][jj]['_'] ] } }, ] } }, ] } }, 2, record.metadata[0]['dc:creator'][jj]['_'], 'bnjson'));
+                    } else if (record.metadata[0]['dc:creator'][jj]['$'].scheme === 'institution') {
+                        promises.push(addRecord({ "article":{ "children":[ { "header":{ "children":[ { "span":{ "children":[ record.metadata[0]['dc:creator'][jj]['_'] ] } }, ] } }, ] } }, 19, record.metadata[0]['dc:creator'][jj]['_'], 'bnjson'));
+                    }
+                }
+            }
+            promises.push(addRecord(rec, 20, rec.accno));
+
+            datastore.query('SELECT id, controlno FROM records', [ ], function (err, results) {
+                if (err) {
+                    throw('unable to get subjects');
+                } else {
+                    for (var ii in results) {
+                        recs[results[ii].controlno] = results[ii].id;
+                    }
+                    console.log(results);
+                    Q.all(promises).then(function (data) {
+                        promises = [];
+                        for (var ii in data) {
+                            if (data[ii].accno) {
+                                recs[data[ii].accno] = data[ii];
+                            } else if (data[ii].article) {
+                                recs[data[ii].article.children[0].header.children[0].span.children[0]] = data[ii].id;
+                            }
+                        }
+                        for (var rec in recs) {
+                            if (typeof recs[rec] === 'object') {
+                                if (recs[rec].creators) {
+                                    for (var ref in recs[rec].creators) {
+                                        ref = recs[rec].creators[ref];
+                                        if (recs[ref]) {
+                                            promises.push(addLink(recs[rec].id, recs[ref], 1, 'By', 'Created'));
+                                        }
+                                    }
+                                }
+                                if (recs[rec].subjects) {
+                                    for (var ref in recs[rec].subjects) {
+                                        ref = recs[rec].subjects[ref];
+                                        console.log(ref);
+                                        if (recs[ref]) {
+                                            promises.push(addLink(recs[rec].id, recs[ref], 8, 'About', 'Topic of'));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        return Q.all(promises);
+                    }).then(function () {
+                        console.log('done');
+                        process.exit();
+                    });
+                }
+            });
+        };
+    });
+});
+
+function addLink(source, target, field, in_label, out_label) {
+    var deferred = Q.defer();
+    datastore.query('INSERT INTO record_links (source_id, target_id, field_id, in_label, out_label, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(), NOW())', [ source, target, field, in_label, out_label ], function (err, results) {
+        if (err) {
+            console.log(err);
+            deferred.reject(err);
+        } else {
+            deferred.resolve(results);
+        }
+    });
+    return deferred.promise;
+}
+
+function addRecord(rec, recordtype, controlno, format) {
+    var deferred = Q.defer();
+    format = format || 'eric';
+    datastore.query('INSERT INTO records (data, recordtype_id, collection_id, controlno, format, created_at, updated_at, deleted) VALUES (?, ?, ?, ?, ?, NOW(), NOW(), 0)', [ JSON.stringify(rec), recordtype, 1, controlno, format ], function (err, results) {
+        if (err) {
+        console.log(err);
+            deferred.reject(err);
+        } else {
+            rec.id = results.insertId;
+            deferred.resolve(rec);
+        }
+    });
+    return deferred.promise;
+}
+
--- bin/importer/ericthesaurus.js
+++ bin/importer/ericthesaurus.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var parser = new xml2js.Parser();
 var recs = { };
 var promises = [ ];
-fs.readFile(__dirname + '/eric/ERICthesaurus.xml', function(err, data) {
+fs.readFile(process.argv[2], function(err, data) {
     parser.parseString(data, function (err, result) {
         var term;
         for (var ii in result.Nstein.Terms[0].Term) {
@@ -40,7 +40,7 @@ fs.readFile(__dirname + '/eric/ERICthesaurus.xml', function(err, data) {
                     rec.preferred = term.Relationships[0].Relationship[jj].Is;
                 }
             }
-            promises.push(addRecord(rec, recordtype));
+            promises.push(addRecord(rec, recordtype, rec.name));
         }
         Q.all(promises).then(function (data) {
             promises = [];
@@ -51,31 +51,31 @@ fs.readFile(__dirname + '/eric/ERICthesaurus.xml', function(err, data) {
                 for (var ref in recs[rec].synonyms) {
                     ref = recs[rec].synonyms[ref];
                     if (recs[ref]) {
-                        promises.push(addLink(recs[rec].id, recs[ref].id, 22));
+                        promises.push(addLink(recs[rec].id, recs[ref].id, 22, 'Preferred term', 'Non-preferred term'));
                     }
                 }
                 for (var ref in recs[rec].broader) {
                     ref = recs[rec].broader[ref];
                     if (recs[ref]) {
-                        promises.push(addLink(recs[rec].id, recs[ref].id, 18));
+                        promises.push(addLink(recs[rec].id, recs[ref].id, 18, 'Narrower', 'Broader'));
                     }
                 }
                 for (var ref in recs[rec].narrower) {
                     ref = recs[rec].narrower[ref];
                     if (recs[ref]) {
-                        promises.push(addLink(recs[rec].id, recs[ref].id, 17));
+                        promises.push(addLink(recs[rec].id, recs[ref].id, 17, 'Broader', 'Narrower'));
                     }
                 }
                 for (var ref in recs[rec].related) {
                     ref = recs[rec].related[ref];
                     if (recs[ref]) {
-                        promises.push(addLink(recs[rec].id, recs[ref].id, 19));
+                        promises.push(addLink(recs[rec].id, recs[ref].id, 19, 'Related', 'Related'));
                     }
                 }
                 for (var ref in recs[rec].preferred) {
                     ref = recs[rec].preferred[ref];
                     if (recs[ref]) {
-                        promises.push(addLink(recs[rec].id, recs[ref].id, 21));
+                        promises.push(addLink(recs[rec].id, recs[ref].id, 21, 'Non-preferred term', 'Preferred term'));
                     }
                 }
             }
@@ -87,9 +87,9 @@ fs.readFile(__dirname + '/eric/ERICthesaurus.xml', function(err, data) {
     });
 });
 
-function addLink(source, target, field) {
+function addLink(source, target, field, in_label, out_label) {
     var deferred = Q.defer();
-    datastore.query('INSERT INTO record_links (source_id, target_id, field_id) VALUES (?, ?, ?)', [ source, target, field ], function (err, results) {
+    datastore.query('INSERT INTO record_links (source_id, target_id, field_id, in_label, out_label, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(), NOW())', [ source, target, field, in_label, out_label ], function (err, results) {
         if (err) {
             deferred.reject(err);
         } else {
@@ -99,9 +99,9 @@ function addLink(source, target, field) {
     return deferred.promise;
 }
 
-function addRecord(rec, recordtype) {
+function addRecord(rec, recordtype, controlno) {
     var deferred = Q.defer();
-    datastore.query('INSERT INTO records (data, recordtype_id, collection_id) VALUES (?, ?, ?)', [ JSON.stringify(rec), recordtype, 1 ], function (err, results) {
+    datastore.query('INSERT INTO records (data, recordtype_id, collection_id, controlno, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW())', [ JSON.stringify(rec), recordtype, 1, controlno ], function (err, results) {
         if (err) {
             deferred.reject(err);
         } else {
