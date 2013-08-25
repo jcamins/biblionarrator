dd5f58e0480007f4ad49f28314487a93b8e9a01d
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-08-24 23:57:18 -0400

    Remove unused searchmap route

--- routes/search.js
+++ routes/search.js
@@ -66,53 +66,6 @@ exports.view = function(req, res) {
     }, function (err) { res.send(404, err); });
 };
 
-exports.searchmap = function (req, res) {
-    var query = req.query.q || '';
-    var facets = [ ];
-    var offset = parseInt(req.query.offset, 10) || 0;
-    var perpage = parseInt(req.query.perpage, 10) || 20;
-
-    if (typeof req.query.facet === 'string') {
-        query = query + ' ' + req.query.facet;
-    } else if (typeof req.query.facet !== 'undefined') {
-        query = query + ' ' + req.query.facet.join(' ');
-    }
-    var queryast = queryparser.qp.parse(query);
-
-    if (req.query.format === 'map') {
-        var records = new g.ArrayList();
-        var recordtypes = new g.ArrayList();
-        var links;
-        if (typeof query === 'object' || query.length === 0) {
-            query = query || null;
-            links = g.V(query).as('me').applyFacets(facets).store(records).bothE().as('links').outV().retain(records).back('links').inV().retain(records).back('links').toJSON();
-        } else {
-            links = graphstore.textSearch(query).as('me').applyFacets(facets).store(records).bothE().as('links').outV().retain(records).back('links').inV().retain(records).back('links').toJSON();
-        }
-        records = g.start(records).as('recs').out('recordtype').property('key').store(recordtypes).back('recs').toJSON();
-        var recmap = { };
-        recordtypes = recordtypes.toJSON();
-        records.forEach(function (rec, index) {
-            records[index].recordtype = recordtypes[index];
-            recmap[rec._id] = index;
-        });
-        var removes = [ ];
-        for (var ii = 0; ii < links.length; ii++) {
-            links[ii].source = recmap[links[ii]._inV];
-            links[ii].target = recmap[links[ii]._outV];
-            if (typeof links[ii].source === 'undefined' || typeof links[ii].target === 'undefined') {
-                removes.unshift(ii);
-            }
-        }
-        removes.forEach(function (ii) {
-            links.splice(ii, 1);
-        });
-        res.json({ records: records, links: links, recmap: recmap });
-        return;
-    }
-
-};
-
 exports.map = function (req, res) {
     if (typeof req.query.landmark === 'string') {
         req.query.landmark = [ req.query.landmark ];
