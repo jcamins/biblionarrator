1a5b62415e896f55c4b683173003cacebc7535c4
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-09-01 11:37:19 -0400

    Start switching to Grunt for building

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            dist: {
+                files: {
+                    'public/js/generated/formathandlers.js': ['clientjs/formathandlers.js'],
+                    'public/js/generated/templates.js': ['clientjs/templates.js'],
+                },
+                options: { }
+            }
+        },
+        less: {
+            dist: {
+                files: {
+                    'public/css/style.css': 'public/css/style.less',
+                }
+            },
+            dist_min: {
+                files: {
+                    'public/css/style.min.css': 'public/css/style.less',
+                },
+                options: { yuicompress: true }
+            },
+        },
+        jshint: {
+            files: [
+                'server.js',
+                'routes/*.js',
+                'models/*.js',
+                'bin/**/*.js',
+                'lib/**/*.js',
+                'tests/*.js'
+            ],
+            options: {
+                "loopfunc": true,
+                "unused": true,
+                "sub": true,
+                global: {
+                    module: true
+                }
+            }
+        },
+        mochaTest: {
+            test: {
+                options: { reporter: 'spec' }
+            },
+            src: [
+                'tests/*.js',
+            ]
+        },
+        coverage: {
+            options: {
+                reporter: 'html-cov',
+                quiet: true,
+                captureFile: 'coverage.html'
+            },
+            src: [ 'tests/*.js' ]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+
+    grunt.registerTask('default', [ 'browserify', 'less' ]);
+};
