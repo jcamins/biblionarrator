0e2b8b7edb2f6c3c801099f8f8ff87cb826f6880
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-09-01 11:44:28 -0400

    Fix queryparser unit test to work properly

--- tests/queryparser.js
+++ tests/queryparser.js
@@ -6,7 +6,7 @@ var decomposedqueries = {
     'whatever title:book': { canonical: '(keyword:whatever && title:book)' },
     'author[smith, james]': {
         canonical: 'author[smith, james]',
-        facets: [ { type: 'author', label: 'Author', value: 'smith, james' } ]
+        facets: [ { type: 'author', label: 'Author', value: 'smith, james', link: 'author%5Bsmith%2C%20james%5D' } ]
     },
     '{{author:smith}}' : { canonical: '{{author:smith}} ' },
     '{{author:smith}} stuff': { canonical: '{{author:smith}} (keyword:stuff)' },
@@ -18,6 +18,41 @@ var expect = require('chai').expect,
     queryparser = require('../lib/queryparser');
 
 describe('Query parser', function () {
+    before(function (done) {
+        queryparser.initialize({
+            operators: {
+                'AND': '&&',
+                'OR': '\\|\\|',
+                'FLOAT_START': '\\{\\{',
+                'FLOAT_END': '\\}\\}',
+                'GS': '\\(',
+                'GE': '\\)',
+                'REQ': '\\+',
+                'DIS': '-',
+                'MOD': '#',
+                'NOT': '!',
+                'FACET_START': '\\[',
+                'FACET_END': '\\]',
+                'FILTER_START': '(range)<',
+                'FILTER_END': '>'
+            },
+            indexes: {
+                author: {
+                    type: 'edge',
+                    unidirected: false
+                },
+                title: {
+                    type: 'text'
+                },
+                keyword: {
+                    type: 'text',
+                    unique: false,
+                    multivalue: false
+                }
+            }
+        });
+        done();
+    });
     Object.keys(decomposedqueries).forEach(function (query) {
         it('decomposes ' + query + ' properly', function () {
             var decomposed = queryparser.decompose(queryparser.parse(query));
