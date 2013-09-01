ae2a896c85ed9f17248afbe03282f3073975a482
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-09-01 17:45:35 -0400

    Fix record model method declarations

--- models/record.js
+++ models/record.js
@@ -29,6 +29,39 @@ function Record(data) {
         }
     };
 
+    /**
+     * Generate a snippet record
+     * @returns {Record} Snippet record
+     */
+    this.snippet = function () {
+        if (typeof this.data === 'string') {
+            this.data = JSON.parse(this.data);
+        }
+        return new Record({
+            id: this.id,
+            data: formatters[this.format].snippet(this.data),
+        });
+    };
+
+    /**
+     * Link the record to another record
+     * @param {string} type type of link to create
+     * @param {Record|string} target Record object or ID of record to link to
+     */
+    this.link = function (type, target) {
+        if (typeof target === 'undefined' || target === null || target === '') {
+            return;
+        }
+        var sv = g.v(this.id).iterator().nextSync();
+        var tv = g.v(typeof target === 'string' ? target : target.id).iterator().nextSync();
+        graphstore.getDB().addEdgeSync(null, sv, tv, type);
+        sv.setPropertySync('vorder', sv.getPropertySync('vorder'));
+        tv.setPropertySync('vorder', tv.getPropertySync('vorder') + 1);
+        if (graphstore.autocommit) {
+            graphstore.getDB().commitSync();
+        }
+    };
+
     this.initialize(data);
 
     if (typeof formatters[this.format] !== 'undefined') {
@@ -37,39 +70,6 @@ function Record(data) {
 
     return this;
 }
-
-/**
- * Generate a snippet record
- * @returns {Record} Snippet record
- */
-Record.prototype.snippet = function () {
-    if (typeof this.data === 'string') {
-        this.data = JSON.parse(this.data);
-    }
-    return new Record({
-        id: this.id,
-        data: formatters[this.format].snippet(this.data),
-    });
-};
-
-/**
- * Link the record to another record
- * @param {string} type type of link to create
- * @param {Record|string} target Record object or ID of record to link to
- */
-Record.prototype.link = function (type, target) {
-    if (typeof target === 'undefined' || target === null || target === '') {
-        return;
-    }
-    var sv = g.v(this.id).iterator().nextSync();
-    var tv = g.v(typeof target === 'string' ? target : target.id).iterator().nextSync();
-    graphstore.getDB().addEdgeSync(null, sv, tv, type);
-    sv.setPropertySync('vorder', sv.getPropertySync('vorder'));
-    tv.setPropertySync('vorder', tv.getPropertySync('vorder') + 1);
-    if (graphstore.autocommit) {
-        graphstore.getDB().commitSync();
-    }
-};
 
 Record.model = 'record';
 
