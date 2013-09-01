9a1fef0c4fc58a74e597129f768ad3b70837d6a9
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-09-01 15:50:13 -0400

    Remove obsolete template model

--- models/index.js
+++ models/index.js
@@ -5,7 +5,6 @@ exports.Query = require('./query');
 exports.Record = require('./record');
 exports.RecordList = require('./recordlist');
 exports.RecordType = require('./recordtype');
-exports.Template = require('./template');
 exports.User = require('./user');
 
 exports.Field.init(exports);
@@ -14,5 +13,4 @@ exports.Query.init(exports);
 exports.Record.init(exports);
 exports.RecordList.init(exports);
 exports.RecordType.init(exports);
-exports.Template.init(exports);
 exports.User.init(exports);
