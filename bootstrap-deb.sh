3c662231b514f498c5bc6b3b859382dff4853ad2
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-09-02 13:40:32 -0400

    Update Vagrant configuration to successfully install BN

--- bootstrap-deb.sh
+++ bootstrap-deb.sh
@@ -2,19 +2,43 @@
 
 apt-get update
 #sudo apt-get upgrade -y
-apt-get install -y python-software-properties
+apt-get install -y python-software-properties equivs
 echo "deb http://debian.datastax.com/community stable main" > /etc/apt/sources.list.d/cassandra.sources.list
 wget -q -O- http://debian.datastax.com/debian/repo_key | apt-key add -
 add-apt-repository ppa:chris-lea/node.js
+add-apt-repository ppa:webupd8team/java
 apt-get update
-apt-get install -y openjdk-6-jdk dsc12 redis-server nodejs maven make g++
-update-alternatives --config java
-echo 'JAVA_HOME="/usr/lib/jvm/java-6-openjdk-amd64"' >> /etc/environment
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+cat >oracle-java-dummy.cfg <<DUMMY_CFG
+Section: misc
+Priority: optional
+Standards-Version: 3.9.2
+
+Package: oracle-java-dummy
+Version: 0.1
+Maintainer: Biblionarrator <biblionarrator@cpbibliography.com>
+Depends: oracle-java7-installer,oracle-java7-set-default
+Provides: java5-runtime-headless
+Description: Dummy package for Java deps
+ Makes Oracle Java provide java5-runtime-headless
+DUMMY_CFG
+equivs-build oracle-java-dummy.cfg
+dpkg -i oracle-java-dummy*.deb
+apt-get install -f -y
+update-java-alternatives -s java-7-oracle
+apt-get install -y redis-server nodejs maven make g++ git vim
 wget -q https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.deb
 dpkg -i elasticsearch-0.90.3.deb
 apt-get install -f -y
-service cassandra start
+RUNLEVEL=1 apt-get install -y dsc12
+update-rc.d cassandra disable
+echo 'manual' > /etc/init/cassandra.override
+cassandra
 npm install -g grunt-cli
 npm install -g node-gyp
-useradd -m -s /bin/bash biblionarrator
-sudo su - biblionarrator -c "cp -R /vagrant /home/biblionarrator/biblionarrator && cd /home/biblionarrator && npm install && grunt"
+sudo su - vagrant -c "cp -R /vagrant /home/vagrant/biblionarrator && cd /home/vagrant/biblionarrator && rm -R node_modules && npm install && grunt build"
+rm oracle-java* elasticsearch*.deb
+rmdir tmp
+echo 'Your VM has been configured. To connect run `vagrant ssh` or `ssh vagrant@192.168.214.214`.'
+echo '*WARNING*: Cassandra will not automatically start if you reboot your VM. You must run `sudo cassandra` in order to start Cassandra'
+echo 'A copy of Biblionarrator has been built in ~/biblionarrator.'
