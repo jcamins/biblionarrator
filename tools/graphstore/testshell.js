e9d8f57c47fc9f3636e8598340d4267d4f6c29dd
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-09-16 23:46:30 -0400

    Update testshell to use environment

--- tools/graphstore/testshell.js
+++ tools/graphstore/testshell.js
@@ -1,30 +1,111 @@
-var opts = {
-    engine: 'titan',
-    titan: {
-        'storage.keyspace': 'bntest',
-        'storage.index.search.backend': 'lucene',
-        'storage.index.search.directory': __dirname + '/data/titanft',
-        'storage.index.search.client-only': false,
-    },
+var config = {
+    graphconf: {
+        engine: 'titan',
+        titan: {
+            'storage.backend': 'cassandra',
+            'storage.hostname': '127.0.0.1',
+            'storage.keyspace': 'bntest',
+            'storage.index.search.backend': 'lucene',
+            'storage.index.search.directory': __dirname + '/data/titanft',
+            'storage.index.search.client-only': false,
+        },
 
-    orient: {
-        path: 'local:' + __dirname + '/data/orient',
-    },
+        orient: {
+            path: 'local:' + __dirname + '/data/orient',
+            "username": "admin",
+            "password": "admin"
+        },
 
-    tinker: {
-        path: null,
-    },
+        tinker: {
+            path: null,
+        },
 
-    neo4j: {
-        path: __dirname + '/data/neo4j',
+        neo4j: {
+            path: __dirname + '/data/neo4j',
+        }
     },
+    operators: {
+       'AND': '&&',
+       'OR': '\\|\\|',
+       'FLOAT_START': '\\{\\{',
+       'FLOAT_END': '\\}\\}',
+       'GS': '\\(',
+       'GE': '\\)',
+       'REQ': '\\+',
+       'DIS': '-',
+       'MOD': '#',
+       'NOT': '!',
+       'FACET_START': '\\[',
+       'FACET_END': '\\]',
+       'FILTER_START': '(range)<',
+       'FILTER_END': '>'
+    },
+    indexes: {
+        author: {
+            type: 'edge',
+            unidirected: false
+        },
+        title: {
+            type: 'text'
+        },
+        keyword: {
+            type: 'text',
+            unique: false,
+            multivalue: false
+        },
+        key: {
+            type: 'property',
+            datatype: 'String',
+            unique: true,
+            multivalue: false
+        },
+        recordtype: {
+            type: 'edge',
+            unidirected: true
+        },
+        model: {
+            type: 'property',
+            datatype: 'String',
+            unique: false,
+            multivalue: false
+        },
+        linkbrowse: {
+            type: 'dbcallback'
+        },
+        vorder: {
+            type: 'property',
+            datatype: 'Integer',
+            unique: false,
+            multivalue: false
+        }
+    },
+    facets: {
+        "author": {
+            "outlabel": "By",
+            "inlabel": "Wrote",
+            "facetlabel": "Author"
+        },
+        "subject": {
+            "outlabel": "About",
+            "inlabel": "Topic of",
+            "facetlabel": "Subject"
+        },
+        "recordtype": {
+            "outlabel": "Is a",
+            "inlabel": "Example of",
+            "facetlabel": "Record type"
+        }
+    }
 };
 
-var graphstore = require('bngraphstore'),
-    g = graphstore(opts),
-    models = require('../models'),
+var expect = require('chai').expect,
+    environment = require('../../src/lib/environment');
+    environment = environment.set(config);
+var graphstore = environment.graphstore,
+    g = graphstore.g,
+    models = require('../../src/models'),
     Query = models.Query,
-    searchengine = require('../lib/searchengine'),
+    searchengine = require('../../src/lib/searchengine'),
     repl = require("repl"),
     inspect = require('eyes').inspector({maxLength: false});
 
@@ -69,6 +150,7 @@ r.context.inspect = inspect;
 r.context.Text = g.java.import('com.thinkaurelius.titan.core.attribute.Text');
 r.context.Query = Query;
 r.context.searchengine = searchengine;
+r.context.environment = environment;
 
 r.on('exit', function () {
   console.log('Good-bye from Gremlin!');
