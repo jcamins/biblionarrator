ad8c60c23af871c434161a2f8ecb3c401d8bc74b
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-10-30 21:47:14 -0400

    Fix jshint exceptions

--- src/lib/environment/mediastore/mongo.js
+++ src/lib/environment/mediastore/mongo.js
@@ -1,9 +1,4 @@
 "use strict";
-var fs = require('fs'),
-    mongodb = require('mongodb'),
-    GridStore = mongodb.GridStore,
-    MongoClient = mongodb.MongoClient,
-    Q = require('q');
 
 function MediaStore(config) {
     var backend = config.backend('mongo');
