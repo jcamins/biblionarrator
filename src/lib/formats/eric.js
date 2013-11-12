56f73570a3ad49bc04fcc68ceccfcc297c834641
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-11-12 09:51:55 -0500

    Use unified importer for ERIC

--- src/lib/formats/eric.js
+++ src/lib/formats/eric.js
@@ -27,7 +27,6 @@ module.exports.indexes = function(recorddata) {
 };
 
 
-/*jshint unused:false */ /* Not yet implemented */
 module.exports.links = function(recorddata) {
     var links = [ ];
     if (recorddata.metadata.dc_creator) {
@@ -99,6 +98,78 @@ module.exports.links = function(recorddata) {
     return links;
 };
 
+/*jshint unused:false */ /* Not yet implemented */
 module.exports.decompile = function(htmldom) {
 };
 /*jshint unused:true */
+
+module.exports.import = function (record, options, maps, matcher) {
+    var rec = { metadata: { } };
+    var newkey, key;
+    for (key in record.metadata) {
+        newkey = key.replace(':', '_');
+        rec.metadata[newkey] = record.metadata[key];
+    }
+    var jj;
+    var identifiers = { };
+    if (rec.metadata.dc_identifier) {
+        rec.metadata.dc_identifier.forEach(function (el) {
+            if (el.$text) {
+                key = el.$.scheme;
+                newkey = key.replace(':', '_');
+                identifiers[newkey] = identifiers[newkey] || [ ];
+                identifiers[newkey].push(el.$text);
+            }
+        });
+        rec.metadata.dc_identifier = identifiers;
+    }
+    var creators = { personal: [ ], institution: [ ] };
+    if (rec.metadata.dc_creator) {
+        rec.metadata.dc_creator.forEach(function (el) {
+            if (el.$text) {
+                if (el.$.scheme === 'personal author') {
+                    creators.personal.push(el.$text);
+                } else if (el.$.scheme === 'institution') {
+                    creators.institution.push(el.$text);
+                } else {
+                    console.log('Unknown creator scheme: ' + el.$.scheme);
+                }
+            }
+        });
+       rec.metadata.dc_creator = creators;
+    }
+    var types = [ ];
+    if (rec.metadata.dc_type) {
+        rec.metadata.dc_type.forEach(function (el) {
+            if (typeof el === 'object' && el !== null) {
+                types.push(el.$text);
+            } else if (typeof el === 'string' && el.length > 0) {
+                types.push(el);
+            }
+        });
+        rec.metadata.dc_type = types;
+    }
+    var subjects = [ ];
+    if (rec.metadata.dc_subject) {
+        rec.metadata.dc_subject.forEach(function (el) {
+            if (typeof el === 'object' && el !== null) {
+                subjects.push(el.$text);
+            } else if (typeof el === 'string' && el.length > 0) {
+                subjects.push(el);
+            }
+        });
+        rec.metadata.dc_subject = subjects;
+    }
+    return { format: 'eric', data: rec, recordclass: 'biblio' };
+};
+
+module.exports.importoptions = {
+    collect: [
+        'dc:creator',
+        'dc:subject',
+        'dc:type',
+        'dc:identifier',
+        'dcterms:educationLevel'
+    ],
+    recordElement: 'record'
+};
