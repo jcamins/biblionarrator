7f69d8fa4c201463ff34b9e97d08f7185cfb335b
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-12-05 09:43:15 -0500

    Use the record as provided to the ERIC handler when not importing raw data

--- src/lib/formats/eric.js
+++ src/lib/formats/eric.js
@@ -103,64 +103,68 @@ module.exports.decompile = function(htmldom) {
 };
 /*jshint unused:true */
 
-module.exports.import = function (record, options, maps, matcher) {
-    var rec = { metadata: { } };
-    var newkey, key;
-    for (key in record.metadata) {
-        newkey = key.replace(':', '_');
-        rec.metadata[newkey] = record.metadata[key];
-    }
-    var jj;
-    var identifiers = { };
-    if (rec.metadata.dc_identifier) {
-        rec.metadata.dc_identifier.forEach(function (el) {
-            if (el.$text) {
-                key = el.$.scheme;
-                newkey = key.replace(':', '_');
-                identifiers[newkey] = identifiers[newkey] || [ ];
-                identifiers[newkey].push(el.$text);
-            }
-        });
-        rec.metadata.dc_identifier = identifiers;
-    }
-    var creators = { personal: [ ], institution: [ ] };
-    if (rec.metadata.dc_creator) {
-        rec.metadata.dc_creator.forEach(function (el) {
-            if (el.$text) {
-                if (el.$.scheme === 'personal author') {
-                    creators.personal.push(el.$text);
-                } else if (el.$.scheme === 'institution') {
-                    creators.institution.push(el.$text);
-                } else {
-                    console.log('Unknown creator scheme: ' + el.$.scheme);
+module.exports.import = function (record, options, maps, matcher, format) {
+    try {
+        var rec = { metadata: { } };
+        var newkey, key;
+        for (key in record.metadata) {
+            newkey = key.replace(':', '_');
+            rec.metadata[newkey] = record.metadata[key];
+        }
+        var jj;
+        var identifiers = { };
+        if (rec.metadata.dc_identifier) {
+            rec.metadata.dc_identifier.forEach(function (el) {
+                if (el.$text) {
+                    key = el.$.scheme;
+                    newkey = key.replace(':', '_');
+                    identifiers[newkey] = identifiers[newkey] || [ ];
+                    identifiers[newkey].push(el.$text);
                 }
-            }
-        });
-       rec.metadata.dc_creator = creators;
+            });
+            rec.metadata.dc_identifier = identifiers;
+        }
+        var creators = { personal: [ ], institution: [ ] };
+        if (rec.metadata.dc_creator) {
+            rec.metadata.dc_creator.forEach(function (el) {
+                if (el.$text) {
+                    if (el.$.scheme === 'personal author') {
+                        creators.personal.push(el.$text);
+                    } else if (el.$.scheme === 'institution') {
+                        creators.institution.push(el.$text);
+                    } else {
+                        console.log('Unknown creator scheme: ' + el.$.scheme);
+                    }
+                }
+            });
+           rec.metadata.dc_creator = creators;
+        }
+        var types = [ ];
+        if (rec.metadata.dc_type) {
+            rec.metadata.dc_type.forEach(function (el) {
+                if (typeof el === 'object' && el !== null) {
+                    types.push(el.$text);
+                } else if (typeof el === 'string' && el.length > 0) {
+                    types.push(el);
+                }
+            });
+            rec.metadata.dc_type = types;
+        }
+        var subjects = [ ];
+        if (rec.metadata.dc_subject) {
+            rec.metadata.dc_subject.forEach(function (el) {
+                if (typeof el === 'object' && el !== null) {
+                    subjects.push(el.$text);
+                } else if (typeof el === 'string' && el.length > 0) {
+                    subjects.push(el);
+                }
+            });
+            rec.metadata.dc_subject = subjects;
+        }
+        return { format: 'eric', data: rec, recordclass: 'biblio' };
+    } catch (e) {
+        return { format: 'eric', data: record, recordclass: 'biblio' };
     }
-    var types = [ ];
-    if (rec.metadata.dc_type) {
-        rec.metadata.dc_type.forEach(function (el) {
-            if (typeof el === 'object' && el !== null) {
-                types.push(el.$text);
-            } else if (typeof el === 'string' && el.length > 0) {
-                types.push(el);
-            }
-        });
-        rec.metadata.dc_type = types;
-    }
-    var subjects = [ ];
-    if (rec.metadata.dc_subject) {
-        rec.metadata.dc_subject.forEach(function (el) {
-            if (typeof el === 'object' && el !== null) {
-                subjects.push(el.$text);
-            } else if (typeof el === 'string' && el.length > 0) {
-                subjects.push(el);
-            }
-        });
-        rec.metadata.dc_subject = subjects;
-    }
-    return { format: 'eric', data: rec, recordclass: 'biblio' };
 };
 
 module.exports.importoptions = {
